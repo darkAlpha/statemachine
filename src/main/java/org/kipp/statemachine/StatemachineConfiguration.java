16d6c5667a20a8b9ec1fd28dbc751beba5983a19
darkAlpha <muramega9@gmail.com>
2025-09-23 23:07:38 +0500

    make as lib format with annoation import

---
+++ src/main/java/org/kipp/statemachine/StatemachineConfiguration.java
@@ -0,0 +1,9 @@
+package org.kipp.statemachine;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class StatemachineConfiguration {
+}
